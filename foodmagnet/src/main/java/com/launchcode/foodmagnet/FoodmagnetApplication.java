b6cffaa82249f6988c30e427444b15ca85b43168
Max Butler <maxgbutler003@gmail.com>
2023-07-10 19:45:56 -0500

    Created Spring-boot project

---
+++ foodmagnet/src/main/java/com/launchcode/foodmagnet/FoodmagnetApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.foodmagnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmagnetApplication.class, args);
+	}
+
+}
---
+++ foodmagnet/src/test/java/com/launchcode/foodmagnet/FoodmagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.foodmagnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
