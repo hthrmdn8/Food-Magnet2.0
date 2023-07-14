5f8632570033c6fbac11c1951058223db86c4a88
Hunter <hacain98@gmail.com>
2023-07-13 20:17:26 -0500

    Added java/com/github/foodmagnet from old project

--- src/main/java/com/launchcode/foodmagnet/FoodmagnetApplication.java
+++ src/main/java/com/launchcode/foodmagnet/FoodmagnetApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodmagnetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodmagnetApplication.class, args);
-	}
 
+		SpringApplication.run(FoodmagnetApplication.class, args);
+
+	}
 }
--- src/main/java/com/launchcode/foodmagnet/controllers/RestaurantController.java
+++ src/main/java/com/launchcode/foodmagnet/controllers/RestaurantController.java
@@ -1,4 +1,15 @@
 package com.launchcode.foodmagnet.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("restaurant")
 public class RestaurantController {
+
+    @GetMapping("")
+    public String index(){
+        return "restaurant";
+    }
 }
