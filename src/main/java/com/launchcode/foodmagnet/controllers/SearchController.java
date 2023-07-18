5c2efec49249783d09d1276d1c42a6be3972231f
Max Butler <maxgbutler003@gmail.com>
2023-07-18 01:45:57 -0500

    routed search.html to /search, added navbar to search.html

--- src/main/java/com/launchcode/foodmagnet/controllers/SearchController.java
+++ src/main/java/com/launchcode/foodmagnet/controllers/SearchController.java
@@ -1,7 +1,18 @@
 package com.launchcode.foodmagnet.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("search")
 public class SearchController {
+
+    @GetMapping
+    public String displaySearchPage(Model model) {
+
+        return "search";
+    }
+
 }
