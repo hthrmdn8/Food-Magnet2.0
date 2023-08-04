9f00b12e4acf1c96da785eb1de8ea3f650161b63
Nisha George <nishajomy.2007@gmail.com>
2023-08-04 11:21:49 -0500

    created UserController and UserRepository

---
+++ src/main/java/com/launchcode/foodmagnet/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.launchcode.foodmagnet.controllers;
+
+import com.launchcode.foodmagnet.dto.UserDto;
+import com.launchcode.foodmagnet.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+@Controller
+public class UserController {
+    @GetMapping("/login")
+    public String login(Model model, UserDto userDto) {
+        model.addAttribute("user", userDto);
+        return "login";
+    }
+
+
+    @GetMapping("/register")
+    public String register(Model model, UserDto userDto) {
+
+        model.addAttribute("user", userDto);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerSave(@ModelAttribute("user") UserDto userDto, Model model) {
+        User user = userService.findByUsername(userDto.getUsername());
+        if (user != null) {
+            model.addAttribute("userexist", user);
+            return "register";
+
+        }
+        userService.save(userDto);
+        return "redirect:/register?success";
+    }
+
+}
---
+++ src/main/java/com/launchcode/foodmagnet/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.launchcode.foodmagnet.repositories;
+
+import com.launchcode.foodmagnet.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername (String username);
+
+}
