86cb4c46c2143151fe03bb4e986b109be73d1fd3
Max Butler <maxgbutler003@gmail.com>
2023-08-05 18:05:16 -0500

    added resource files to git, created fragments.html & refactored the head and navbar into fragments, renamed hello.html into city.html & mapped the page with MapController.java

--- src/main/java/com/launchcode/foodmagnet/controllers/MapController.java
+++ src/main/java/com/launchcode/foodmagnet/controllers/MapController.java
@@ -1,4 +1,16 @@
 package com.launchcode.foodmagnet.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("city")
 public class MapController {
+
+    @GetMapping
+    public String renderCityPage() {
+
+        return "city";
+    }
 }
