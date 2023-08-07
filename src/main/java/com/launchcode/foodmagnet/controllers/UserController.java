a0d2f960252c5a0838cecb0f6b949de07af1ad7a
Nisha George <nishajomy.2007@gmail.com>
2023-08-07 12:05:33 -0500

    fixed UserController autowiring UserService object

--- src/main/java/com/launchcode/foodmagnet/controllers/UserController.java
+++ src/main/java/com/launchcode/foodmagnet/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.launchcode.foodmagnet.controllers;
 
 import com.launchcode.foodmagnet.dto.UserDto;
 import com.launchcode.foodmagnet.models.User;
+import com.launchcode.foodmagnet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    private UserService userService;
+    public UserController(UserService userService) {
+
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
     public String login(Model model, UserDto userDto) {
         model.addAttribute("user", userDto);
