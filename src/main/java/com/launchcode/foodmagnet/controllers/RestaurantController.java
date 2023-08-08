720456555a0e56755dd3916c2ebb0be83304156e
Nisha George <nishajomy.2007@gmail.com>
2023-08-08 18:36:29 -0500

    changed restaurant controller and restaurant.html

--- src/main/java/com/launchcode/foodmagnet/controllers/RestaurantController.java
+++ src/main/java/com/launchcode/foodmagnet/controllers/RestaurantController.java
@@ -1,15 +1,28 @@
 package com.launchcode.foodmagnet.controllers;
 
+import com.launchcode.foodmagnet.models.data.RestaurantData;
+import com.launchcode.foodmagnet.models.restaurant.Restaurant;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
-@RequestMapping("restaurant")
 public class RestaurantController {
 
-    @GetMapping("")
-    public String index(){
-        return "restaurant";
+    @GetMapping("/restaurant")
+    public String showRestaurantDetails(@RequestParam("placeId") String placeId, Model model) {
+        // Here, you can fetch the restaurant details using the placeId
+        // and pass the details to the view template using the Model
+        // For example:
+        Restaurant restaurant = RestaurantData.getRestaurantDetails(placeId);
+        model.addAttribute("restaurant", restaurant);
+
+        return "restaurant"; // Create a new Thymeleaf template named "restaurant_details.html"
     }
 }
