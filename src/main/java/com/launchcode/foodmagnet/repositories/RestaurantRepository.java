a3597fbdf18f2a78e1082b35d9b1e14e8c5eb60e
Nisha George <nishajomy.2007@gmail.com>
2023-08-14 09:52:31 -0500

    added RestaurantRepository

--- src/main/java/com/launchcode/foodmagnet/repositories/RestaurantRepository.java
+++ src/main/java/com/launchcode/foodmagnet/repositories/RestaurantRepository.java
@@ -1,4 +1,12 @@
 package com.launchcode.foodmagnet.repositories;
 
-public interface RestaurantRepository {
+import com.launchcode.foodmagnet.models.RestaurantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<RestaurantEntity,Integer> {
+
+
+    RestaurantEntity findByName(String name);
 }
