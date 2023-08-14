767653df3733ca8369e37e785ca45dadb732ca46
Nisha George <nishajomy.2007@gmail.com>
2023-08-14 09:53:24 -0500

    added ReviewRepository

--- src/main/java/com/launchcode/foodmagnet/repositories/ReviewRepository.java
+++ src/main/java/com/launchcode/foodmagnet/repositories/ReviewRepository.java
@@ -1,4 +1,16 @@
 package com.launchcode.foodmagnet.repositories;
 
-public interface ReviewRepository {
+
+import com.launchcode.foodmagnet.models.RestaurantEntity;
+import com.launchcode.foodmagnet.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review,Integer> {
+    List<Review> findByRestaurantEntity(RestaurantEntity restaurantEntity);
+
+    List<Review> findByCreatedByUser(String createdByUser);
 }
