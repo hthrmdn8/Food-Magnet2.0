e2b5c1029f095fd0d845bb1f446606437f6ccea0
Nisha George <nishajomy.2007@gmail.com>
2023-08-14 09:55:58 -0500

    created UpdateReview model

--- src/main/java/com/launchcode/foodmagnet/models/Review.java
+++ src/main/java/com/launchcode/foodmagnet/models/Review.java
@@ -1,2 +1,84 @@
-package com.launchcode.foodmagnet.models;public class Review {
+package com.launchcode.foodmagnet.models;
+
+import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue
+    private int reviewId;
+    @NonNull
+    private int ratings;
+    private String comments;
+
+    private String createdByUser;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_restaurantId")
+    private RestaurantEntity restaurantEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    public Review() {
+    }
+
+    public Review(int reviewId, int ratings, String comments, String createdByUser, RestaurantEntity restaurantEntity, User user) {
+        this.reviewId = reviewId;
+        this.ratings = ratings;
+        this.comments = comments;
+        this.createdByUser = createdByUser;
+        this.restaurantEntity = restaurantEntity;
+        this.user = user;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public int getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(int ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getCreatedByUser() {
+        return createdByUser;
+    }
+
+    public void setCreatedByUser(String createdByUser) {
+        this.createdByUser = createdByUser;
+    }
+
+    public RestaurantEntity getRestaurantEntity() {
+        return restaurantEntity;
+    }
+
+    public void setRestaurantEntity(RestaurantEntity restaurantEntity) {
+        this.restaurantEntity = restaurantEntity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/launchcode/foodmagnet/models/UpdateReview.java
+++ src/main/java/com/launchcode/foodmagnet/models/UpdateReview.java
@@ -1,2 +1,39 @@
-package com.launchcode.foodmagnet.models;public class UpdateReview {
+package com.launchcode.foodmagnet.models;
+
+public class UpdateReview {
+
+    private int updateReviewId;
+    private int rating;
+    private String comments;
+    public UpdateReview(){}
+
+    public UpdateReview(int updateReviewId, int rating, String comments) {
+        this.updateReviewId = updateReviewId;
+        this.rating = rating;
+        this.comments = comments;
+    }
+
+    public int getUpdateReviewId() {
+        return updateReviewId;
+    }
+
+    public void setUpdateReviewId(int updateReviewId) {
+        this.updateReviewId = updateReviewId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
