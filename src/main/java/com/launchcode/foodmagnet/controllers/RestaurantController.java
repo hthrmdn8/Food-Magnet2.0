c66e983fc49c3e16d2091facf5ce79edf4a000d4
Hunter <hacain98@gmail.com>
2023-08-19 20:45:41 -0500

    Formatted restaurant page photos

--- src/main/java/com/launchcode/foodmagnet/controllers/RestaurantController.java
+++ src/main/java/com/launchcode/foodmagnet/controllers/RestaurantController.java
@@ -11,15 +11,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("restaurant")
-public class RestaurantController {
+//@RequestMapping("restaurant")
+//public class RestaurantController {
+//
+//    @GetMapping("")
+//    public String index(Model model, @RequestParam String place_id){
+//
+//        Restaurant restaurant = RestaurantData.getRestaurantDetails(place_id);
+//
+//        model.addAttribute("title", restaurant.getName());
+//        return "restaurant";
+//    }
 
-    @GetMapping("")
-    public String index(Model model, @RequestParam String place_id){
+public class RestaurantController {
+@GetMapping("/restaurant")
+public String showRestaurantDetails(@RequestParam("place_id") String place_id, Model model) {
+
+        // Here, you can fetch the restaurant details using the placeId
+        // and pass the details to the view template using the Model
 
         Restaurant restaurant = RestaurantData.getRestaurantDetails(place_id);
+        model.addAttribute("restaurant", restaurant);
 
-        model.addAttribute("title", restaurant.getName());
-        return "restaurant";
-    }
+//        restaurantEntity.setName(restaurant.getName());
+//        restaurantRepository.save(restaurantEntity);
+        //RestaurantEntity restaurantEntity =  restaurantRepository.findByName(restaurant.getName());
+//if(restaurantEntity!=null){
+
+        //restaurantRepository.findByName(restaurant.getName())
+//model.addAttribute("restaurantName",restaurantEntity.getName());
+        //List<Review> reviews = reviewRepository.findByRestaurantEntity(restaurantEntity);
+        //model.addAttribute("reviews", reviews);}
+
+        model.addAttribute("place_id", place_id);
+        return "restaurant"; // Create a new Thymeleaf template named "restaurant_details.html"
+
+        }
+
 }
